4a7752da90f0e56e1f33abb2895d94c38465dc3b
Araozu <faraoz@unsa.edu.pe>
2020-12-01 10:56:03 -0500

    Agregar respuesta dia 1 puzzle 1 con procesos

--- elixir/lib/dia01.ex
+++ elixir/lib/dia01.ex
@@ -1,6 +1,33 @@
 defmodule Advent.Dia01 do
   @moduledoc false
 
+  defp find2p(value, numbers) do
+    pidActual = self()
+    numElems = Enum.count(numbers)
+    indices = 0..numElems
+    Enum.each(indices, fn i ->
+      spawn fn ->
+        item = Enum.at(numbers, i)
+        nuevosIndices = (i + 1)..numElems
+
+        Enum.each(nuevosIndices, fn j ->
+          case Enum.at(numbers, j, nil) do
+            nil -> 0
+            nItem ->
+              if item + nItem == value do
+                send(pidActual, {:ok, item, nItem})
+              end
+          end
+        end)
+
+      end
+    end)
+
+    receive do
+      {:ok, v1, v2} -> IO.puts "Encontrado con procesos: #{v1}, #{v2} -> #{v1 * v2}"
+    end
+  end
+
   defp find2(value, numbers) do
     case numbers do
       [x | xs] ->
@@ -15,6 +42,9 @@ defmodule Advent.Dia01 do
   def puzzle1(rutaArchivo) do
     {:ok, str} = File.read(rutaArchivo)
     numeros = Enum.map(String.split(str, "\n"), &(String.to_integer(&1)))
+
+    find2p(2020, numeros)
+
     strRes =
       case find2(2020, numeros) do
         {:ok, v1, v2} -> "El resultado es #{v1 * v2}"
